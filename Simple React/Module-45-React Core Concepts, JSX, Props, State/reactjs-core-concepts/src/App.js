345a666382102e753b1e7f93f03f714d31f4365c
Reja Kazi <rejakazi02@gmail.com>
2022-05-11 23:47:41 +0600

    Jsx work

--- Simple React/Module-45-React Core Concepts, JSX, Props, State/reactjs-core-concepts/src/App.js
+++ Simple React/Module-45-React Core Concepts, JSX, Props, State/reactjs-core-concepts/src/App.js
@@ -1,11 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 
+const id = 555;
+const singer1 = { name: 'Dr. Mahfuzur ', job : 'Singer1'} 
+const singer2 = { name: 'Eva Rahman ', job : 'Singer2'} 
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>JSX</h1>
+       <div className="contrainer">
+
+         <h3>hello dud ds</h3>
+       </div>
+
+       <div className="music">
+         <p>Id: {id}</p>
+         <p>Name: {singer1.name} Job: {singer1.job}</p>
+         <p>Name: {singer2.name} Job: {singer2.job}</p>
+       </div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
