f1f4b7918325b0c1af0224ca0010fd0b85a34d5f
Reja Kazi <rejakazi02@gmail.com>
2022-05-12 16:44:10 +0600

    Load dynamic data, API call useEffect integrate state

--- Simple React/Module-45-React Core Concepts, JSX, Props, State/reactjs-core-concepts2/src/App.js
+++ Simple React/Module-45-React Core Concepts, JSX, Props, State/reactjs-core-concepts2/src/App.js
@@ -1,11 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   return (
     <div className="App">
-      <Counts> </Counts>{" "}
+      <Counts> </Counts>
+      <ExternalUsers></ExternalUsers>
     </div>
   );
 }
@@ -13,20 +14,52 @@ function App() {
 function Counts(props) {
   const [count, setCount] = useState(50);
 
-  // 1 away 
+  // 1 away
 
   const increaseCount = () => {
     const newCount = count + 1;
     setCount(newCount);
   };
 
-  // 2 away 
+  // 2 away
   const decreaseCount = () => setCount(count - 1);
   return (
     <div className="count">
       <h1> Counts: {count} </h1>{" "}
       <button onClick={increaseCount}> Increase </button>{" "}
       <button onClick={decreaseCount}> Decrease </button>{" "}
+    </div>
+  );
+}
+
+// ExternalUsers section
+
+function ExternalUsers() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
+
+  return (
+    <div>
+      <h2>External Users</h2>
+
+      {users.map((user) => (
+        <User name={user.name} email={user.email}>
+          {" "}
+        </User>
+      ))}
+    </div>
+  );
+}
+
+function User(props) {
+  return (
+    <div className="userDiv">
+      <h3>Name: {props.name}</h3>
+      <p>Email: {props.email}</p>
     </div>
   );
 }
