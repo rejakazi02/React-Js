e3fccf3bd44702857400cf892c16015295f7c70c
Reja Kazi <rejakazi02@gmail.com>
2022-05-12 16:07:15 +0600

    State, component state hook and set state method  Increase and Decrease

--- Simple React/Module-45-React Core Concepts, JSX, Props, State/reactjs-core-concepts2/src/App.js
+++ Simple React/Module-45-React Core Concepts, JSX, Props, State/reactjs-core-concepts2/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counts> </Counts>{" "}
+    </div>
+  );
+}
+
+function Counts(props) {
+  const [count, setCount] = useState(50);
+
+  // 1 away 
+
+  const increaseCount = () => {
+    const newCount = count + 1;
+    setCount(newCount);
+  };
+
+  // 2 away 
+  const decreaseCount = () => setCount(count - 1);
+  return (
+    <div className="count">
+      <h1> Counts: {count} </h1>{" "}
+      <button onClick={increaseCount}> Increase </button>{" "}
+      <button onClick={decreaseCount}> Decrease </button>{" "}
     </div>
   );
 }
