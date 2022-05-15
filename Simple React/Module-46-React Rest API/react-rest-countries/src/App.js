1e35ae201846074eba51757ffb01ab3a08261efd
Reja Kazi <rejakazi02@gmail.com>
2022-05-15 18:21:09 +0600

    print contry name

--- Simple React/Module-46-React Rest API/react-rest-countries/src/App.js
+++ Simple React/Module-46-React Rest API/react-rest-countries/src/App.js
@@ -19,14 +19,28 @@ fetch("https://restcountries.com/v3.1/all")
 .then(data => setCountries(data))
 
 
-},[])
+},[]);
   return (
     
     <div>
       <h1>Visiting Every of the world!!!</h1>
 <h2>Available countries:  {countries.length}</h2>
+ 
+ {
+   countries.map(country => <CountriesInfo name={country.name.common} > </CountriesInfo>)
+ }
+ 
     </div>
   )
 }
 
+
+function CountriesInfo(props) {
+  return (
+
+    <div> 
+      <h1> Name {props.name}</h1>
+    </div>
+  )
+  }
 export default App;
